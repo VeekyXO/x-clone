06ca06bd49799d5d339f946aac360cd5927f4365
VeekyXO <67895215+VeekyXO@users.noreply.github.com>
2024-02-25 10:25:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// Function to open the modal
+function openCreateAccountModal() {
+    var modal = document.getElementById("createAccountModal");
+    modal.style.display = "block";
+}
+
+// Function to close the modal
+function closeCreateAccountModal() {
+    var modal = document.getElementById("createAccountModal");
+    modal.style.display = "none";
+}
